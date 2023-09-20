18285aca9b03b2360b267a8619d6e3cd194fcb5b
Mahdi Asadolahzade <mahdiasadi140@gmail.com>
2023-09-20 17:07:26 +0330

    Update Database

--- database.js
+++ database.js
@@ -1,13 +1,11 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  password: "Mahdi111",
-  host: "localhost",
-  port: 5432,
+  user: "root",
+  password: "pmQhDiwsCiqMM5OoRO0dtLuA",
+  host: "robin.iran.liara.ir",
+  port: 32271, 
   database: "login_system"
 });
-
-
 
 module.exports = pool;
